00a2f210c5638178647ded83498fe57fc6bb2a0d
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-03-17 06:14:09 +0600

    Add service for auth.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../users/entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { AccessToken } from './types/AccessToken';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: LoginDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(loginDto: LoginDto): Promise<User> {
+    const user: User = await this.usersService.findOneByUsername(
+      loginDto.username,
+    );
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    const isMatch: boolean = bcrypt.compareSync(
+      loginDto.password,
+      user.password,
+    );
+    if (!isMatch) {
+      throw new BadRequestException('Password does not match');
+    }
+
+    return user;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(user: User): Promise<AccessToken> {
+    const payload = { email: user.email, id: user.id };
+    return { access_token: this.jwtService.sign(payload) };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  async register(registerDto: RegisterDto): Promise<AccessToken> {
+    const existingUser = await this.usersService.findOneByEmail(
+      registerDto.email,
+    );
 
-  update(id: number, updateAuthDto: RegisterDto) {
-    return `This action updates a #${id} auth`;
-  }
+    if (existingUser) {
+      throw new BadRequestException('email already exists');
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+    const newUser: { password: string; email: string; username: string } = {
+      ...registerDto,
+      password: hashedPassword,
+    };
+    const user: User = await this.usersService.create(newUser);
+
+    return this.login(user);
   }
 }
